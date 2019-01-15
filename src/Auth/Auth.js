8c5c12f64a8f834fbd22371a096525fb3fdbca1d
Sam Gaulding <sgaulding@users.noreply.github.com>
2019-01-15 13:05:01 -0600

    feat(Auth0): Store/Retrieve Info in Local Storage for Login

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -16,5 +16,30 @@ export default class Auth {
     this.auth0.authorize();
   };
 
-  handleAuthentication = () => {};
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        alert(`Error: ${err.error}. \n Check the console for further details`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = authResult => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+  };
+
+  isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    return new Date().getTime() < expiresAt;
+  }
 }
